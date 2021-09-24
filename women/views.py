26baefc4b0e1d601691b02f07edd2bfc256ec84f
nazar <nazar.khort@gmail.com>
2021-09-24 14:33:22 +0300

    Added two simple views and created routing for them.

--- women/views.py
+++ women/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Сторінка women.")
+
+
+def categories(request):
+    return HttpResponse("<h1>Статті по категоріях</h1>")
