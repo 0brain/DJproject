ece161934779d8e6828b7d8bedb436b0a4a2cabc
nazar <nazar.khort@gmail.com>
2021-09-25 22:56:06 +0300

    Changed debug mod to ON. Installed Pillow and made migrations.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p4r-h&hie!nirf_7#e!90gnw!5)0hag#=&^=w^)oa1c$zkj=0h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
