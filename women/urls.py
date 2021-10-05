d48680b974e791c0e66ecac310450de27efb5227
nazar <nazar.khort@gmail.com>
2021-10-05 14:03:22 +0300

    Added urlpatterns in accordance to previously created views and connected url to menu items in case.html.

--- women/urls.py
+++ women/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
 ]
