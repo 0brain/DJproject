2c9194dd553050b9af55fa0d8dc20990c149a3cf
nazar <nazar.khort@gmail.com>
2021-10-10 15:03:18 +0300

    Created Meta class for Women model, added ordering for items and verbose name for Women model. Also added verbose names to all fields in Women model.

--- women/models.py
+++ women/models.py
@@ -3,19 +3,24 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Контент статті")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Дата редагування")
+    is_published = models.BooleanField(default=True, verbose_name="Чи опубліковано")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категорії")
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Відомі жінки'
+        verbose_name_plural = 'Відомі жінки'
+        ordering = ['time_create', 'title']
 
 
 class Category(models.Model):
